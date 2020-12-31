2b6c5475dcf455a188604460f34aa7a3a75141cb
Tomasz Cichocki <cichymail@gmail.com>
2020-12-31 18:48:39 +0100

    Speed limit

--- robot_arm.ino
+++ robot_arm.ino
@@ -1,33 +1,58 @@
 #include <Servo.h>
+#include <Ticker.h>
 
 const uint8_t sn = 4;
 Servo s[sn];
 const int s_pin[sn] = {13, 12, 14, 16};
 
+Ticker ticker;
+
 /*
- * Position range is -50..50 = -90 deg..90 deg
- * Max speed is 3ms per position.
+ * Position range is -100..100 = -90 deg..90 deg
+ * Max speed is 1.5ms per position.
  * Position can be updated every 20 ms
- * So max speed is 7 positions per 20 ms.
- * Min speed is 1 position per 20 ms (we want to do some move after all?)
+ * So max speed is 13.3 positions per 20 ms.
+ * 
+ * position is stored in us (sent to servo)
+ * speed is position difference per 20ms
  */
+const int max_speed = 140;
+int current_position[sn];
+int target_position[sn];
+int speed_limit[sn] = {max_speed,max_speed,max_speed,max_speed};
+
+void update_servos() {
+  for (int i = 0; i < sn; ++i) {
+    int diff = target_position[i]-current_position[i];
+    if (diff != 0) {
+      if (diff > 0) {
+        if (diff > speed_limit[i]) {
+          diff = speed_limit[i];
+        }
+      } else {
+        if (diff < -speed_limit[i]) {
+          diff = -speed_limit[i];
+        }
+      }
+      current_position[i] += diff;
+      s[i].writeMicroseconds(DEFAULT_PULSE_WIDTH + current_position[i]);
+    }
+  }
+}
 
 String inputString = "";
 bool stringComplete = false;
 
-// why isn't it available from Servo.h ?
-#define DEFAULT_NEUTRAL_PULSE_WIDTH 1500
-
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
-  delay(10);
+  Serial.begin(115200);
   Serial.println('\n');
   inputString.reserve(100);
 
   for (size_t i = 0; i < sn; ++i) {
     s[i].attach(s_pin[i]);
   }
+
+  ticker.attach_ms(20, update_servos);
 }
 
 void loop() {
@@ -36,12 +61,24 @@ void loop() {
   }
   if (stringComplete) {
     byte servo_no = inputString[0] - 'A';
+    int separator = inputString.indexOf(',');
+
     long pos = inputString.substring(1).toInt();
+    target_position[servo_no] = 10*pos; //position is stored in us (sent to servo)
+    
+    long sp;
+    if (separator > 0) {
+      sp = inputString.substring(separator+1).toInt();
+    } else {
+      sp = max_speed;
+    }
+    speed_limit[servo_no] = sp;
 
     Serial.print((char)('A'+servo_no));
-    Serial.print(':');
-    Serial.println(pos);
-    s[servo_no].writeMicroseconds(DEFAULT_NEUTRAL_PULSE_WIDTH+10*pos);
+    Serial.print(',');
+    Serial.print(pos);
+    Serial.print(',');
+    Serial.println(sp);
 
     inputString = "";
     stringComplete = false;
